deef3e943b3e38fdffb509cabdc84f25b3e031ee
Noam <noamasher55@gmail.com>
2025-01-07 20:20:02 +0200

    test

--- src/Ex2Tests.java
+++ src/Ex2Tests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +27,6 @@ class Ex2Tests {
         for (String form : validFormulas) {
             assertTrue(cell.isForm(form));
         }
-        System.out.println("------------------------------------------------");
         String[] invalidFormulas = {
                 "=a", "=AB", "=@2", "=2+)", "=(3+1*2)-", "==1", "=1+", "=A1+2+3)", "=(1+A2", "=A1+(2-3",
                 "=1*2+", "=(1/2", "=1/(2", "=1*2/3+)", "=1+2-3/", "=(1+2)*3-)", "=1-2+", "=(A1+A2-", "=1+2+3)+4",
@@ -45,16 +45,102 @@ class Ex2Tests {
         }
     }
 
+    @Test
+    void testISIn() throws Exception {
+        Ex2Sheet sheet = new Ex2Sheet();
+        int x = 8,y=15;
+        assertTrue(sheet.isIn(x, y));
+        x = -1;
+        y = 5;
+        assertFalse(sheet.isIn(x, y));
+        x = 15;
+        y = -7;
+        assertFalse(sheet.isIn(x, y));
+        x = 25;
+        y = 100;
+        assertFalse(sheet.isIn(x, y));
+    }
 
     @Test
-    void testValidDependencies() throws Exception {
-        Ex2Sheet mockSheet = new Ex2Sheet();
-        mockSheet.set(0,0,"=12");
-        mockSheet.set(0,1,"=A0+2");
-        mockSheet.set(0,2,"=A1+A0");
-        int[][] depth = mockSheet.depth();
-        assertEquals(2, depth[0][2], "Depth should correctly compute as 2 for valid dependencies.");
+    void testValue()throws Exception{
+        Ex2Sheet sheet = new Ex2Sheet();
+        sheet.set(0,0,"=15+2");
+        assertEquals("17.0",sheet.value(0,0));
+        sheet.set(0,0,"=15G+2");
+        assertEquals(Ex2Utils.ERR_FORM,sheet.value(0,0));
+        sheet.set(0,0,"=18/9*3");
+        assertEquals("6.0",sheet.value(0,0));
     }
+
+    @Test
+    void testCompute() throws Exception {
+        String s1 = "((4+2)*2/4)+35";
+        SCell cell = new SCell(s1, new Ex2Sheet());
+        assertEquals(38,cell.computeForm(cell.getData()));
+        cell.setData("(((4+2)*2/4)+35)/2");
+        assertEquals(19,cell.computeForm(cell.getData()));
+        cell.setData("=(50-30)/(2+4)");
+        assertEquals(3.3333333333333335,cell.computeForm(cell.getData()));
+        cell.setData("=(8+(3*(4+2)))-(7/(5-2))");
+        assertEquals(23.666666666666668,cell.computeForm(cell.getData()));
+    }
+
+
+    @Test
+    void testIsVaild() throws Exception {
+        CellEntry entry = new CellEntry("A40");
+        assertTrue(entry.isValid());
+        entry.setIndex("Z30");
+        assertTrue(entry.isValid());
+        entry.setIndex("G400");
+        assertFalse(entry.isValid());
+        entry.setIndex("B");
+        assertFalse(entry.isValid());
+        entry.setIndex("$15");
+        assertFalse(entry.isValid());
+        entry.setIndex("A##");
+        assertFalse(entry.isValid());
+        entry.setIndex("B-1");
+        assertFalse(entry.isValid());
+    }
+    @Test
+    void testGetXY()throws Exception {
+        CellEntry entry = new CellEntry("A40");
+        assertEquals(0,entry.getX());
+        assertEquals(40,entry.getY());
+        entry.setIndex("Z30");
+        assertEquals(25,entry.getX());
+        assertEquals(30,entry.getY());
+        entry.setIndex("G15");
+        assertEquals(6,entry.getX());
+        assertEquals(15,entry.getY());
+    }
+
+    @Test
+    void testDepth() throws Exception {
+        Ex2Sheet sheet = new Ex2Sheet();
+        sheet.set(0,0,"=A0");
+        sheet.set(0,1,"=A0");
+        sheet.set(0,2,"=3");
+        sheet.set(1,0,"=3");
+        sheet.set(1,1,"=B0");
+        sheet.set(1,2,"=B1");
+        sheet.set(1,0,"=B2");
+        System.out.println(2);
+        sheet.eval();
+        int[][] dd = sheet.depth();
+        assertEquals(-1,dd[0][0]);
+        assertEquals(-1,dd[0][1]);
+        assertEquals(0,dd[0][2]);
+        System.out.println("1");
+        assertEquals(-1,dd[1][0]);
+        assertEquals(-1,dd[1][1]);
+        assertEquals(-1,dd[1][2]);
+
+
+    }
+
+
 
     @Test
     void evalTest() {
